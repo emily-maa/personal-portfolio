5ac00f20733bf28cb26c3cb20e10060dda5f8891
emily-maa <emilymaa@umich.edu>
2024-08-09 13:10:18 -0500

    Changed navbar layout

--- src/sections/Navigation/Navigation.jsx
+++ src/sections/Navigation/Navigation.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styles from './NavigationStyles.module.css'
 
 export const Navigation = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <nav className={styles.navbar}>
-      <ul className={styles.navlist}>
-        <li className={styles.navitem}>
-          <a href="#hero">Home</a>
-        </li>
-        <li className={styles.navitem}>
-          <a href="#projects">Projects</a>
-        </li>
-        <li className={styles.navitem}>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+        <div class={styles.hamburger} onClick={toggleMenu}>&#9776;</div>
+        <ul className={`${styles.navlist} ${isMenuOpen ? styles.active : ''}`}>
+        {/* <ul className={styles.navlist}> */}
+            <li className={styles.navitem}>
+                <a href="#hero">Home</a>
+            </li>
+            <li className={styles.navitem}>
+                <a href="#projects">Projects</a>
+            </li>
+            <li className={styles.navitem}>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
     </nav>
   );
 }
